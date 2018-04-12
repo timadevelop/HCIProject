aeccbf199c13ef8677178f97f14b8f0f1547d35c
vlad <timadevelop@gmail.com>
2018-04-12 22:04:06 +0300

    merged with other ver

--- UI/gulpfile.js
+++ UI/gulpfile.js
@@ -2,28 +2,46 @@
 
 var   gulp = require('gulp'),
     concat = require('gulp-concat'),
-       del = require('del');
+       del = require('del'),
+livereload = require('gulp-livereload'),
+      sass = require('gulp-sass');
 
-var pages = ['index.html'];
 
-var src = './src';
-var partials_src = src + '/partials/';
+var pages = ['index.html', 'login.html', 'me.html', 'about.html'];
+
+var partials_src = 'partials/';
+
+gulp.task('compileSass', function() {
+  return gulp.src("scss/minimal.scss")
+      .pipe(sass())
+      .pipe(gulp.dest('public/css'))
+      .pipe(livereload());
+});
+
 
 gulp.task("concatPages", function() {
   pages.forEach((page) => {
     gulp.src([partials_src + 'header.html', partials_src + page, partials_src + 'footer.html'])
     .pipe(concat(page))
-    .pipe(gulp.dest("./src/public"));
+    .pipe(gulp.dest('public'))
+    .pipe(livereload());
   });
 });
 
+gulp.task("watch", function() {
+  livereload.listen();
+  return gulp.watch(['partials/*.html', 'scss/*', 'img/*', 'js/*'], ['build'])
+})
+
 gulp.task('clean', function() {
-  del(['./src/public'])
+  del(['public'])
 });
 
-gulp.task('build', ['concatPages'], function() {
-  return gulp.src([src + '/css/*', src + '/img/*', src + '/js/*'], { base: './src'})
-  .pipe(gulp.dest('./src/public'));   
+gulp.task("build", ['compileSass', 'concatPages'], function() {
+  return gulp.src(["css/*", "img/*", "js/*"], { base: './'})
+  .pipe(gulp.dest('public'))
+  .pipe(livereload());
+
 });
 
-gulp.task("default", ["build"]);
+gulp.task("default", ["watch"]);
