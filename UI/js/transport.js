f726be1767444061a5496cf1fea5ed9d435bee07
timadevelop <timadevelop@gmail.com>
2018-06-18 03:47:29 +0300

    fix transport type init filter

--- UI/js/transport.js
+++ UI/js/transport.js
@@ -13,29 +13,18 @@ function filter(value) {
 
 window.onload = function() {
 
-  filter('own');
+  waitForElement(() => initTransport());
 
-waitForElement(() => initTransport());
+  function deleteFromScenario(e) {
+    // update scenario
 
-function deleteFromScenario(e) {
-  // update scenario
+    alert('TODO: delete ' + e + ' from scenario');
+  }
 
-  alert('TODO: delete ' + e + ' from scenario');
-}
-
-function initTransport() {
-  console.log('transport_item init')
-  const selection = document.getElementById('transport_selection');
-  selection.value = global_data.transport;
-  // global_data.scenario = [...global_data.scenario, "add new one"]; // add new element
-  // global_data.scenario.forEach((e) => {
-  //   let s = `
-  //       <li class="flex space-between horizontal">
-  //         <input type="text" value="${e}" />
-  //         <i onClick="deleteFromScenario('${e}')" class="fa fa-trash"></i>
-  //       </li>
-  //     `;
-  //   list.appendChild(createElementFromHTML(s));
-  // });
-}
+  function initTransport() {
+    console.log('transport_item init')
+    const selection = document.getElementById('transport_selection');
+    selection.value = global_data.transport;
+    filter(global_data.transport);
+  }
 }
